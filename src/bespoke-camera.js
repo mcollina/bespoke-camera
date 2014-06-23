1d6f8f78278b8800892086d17c23e8b42549bd96
Matteo Collina <matteo.collina@gmail.com>
2014-06-23 10:51:00 +0200

    Fixed behavior with multiple slides with a camera.

--- src/bespoke-camera.js
+++ src/bespoke-camera.js
@@ -35,8 +35,9 @@ bespoke.plugins.camera = function(deck, options) {
     if (e.slide.getAttribute('data-camera') === 'fullscreen') {
       show()
       requestFullscreen()
-    } if (e.slide.getAttribute('data-camera') !== null) {
+    } else if (e.slide.getAttribute('data-camera') !== null) {
       show()
+      exitFullscreen()
     } else {
       hide()
       setTimeout(exitFullscreen, 500);
@@ -64,13 +65,14 @@ bespoke.plugins.camera = function(deck, options) {
                        navigator.msGetUserMedia);
 
     var constraints = {
-      video: {
-       // HD required, then reducing via CSS
-       mandatory: {
-          minWidth: 1280,
-          minFrameRate: 30.0
-        }
-      },
+      //video: {
+      // // HD required, then reducing via CSS
+      // mandatory: {
+      //    minWidth: 1280,
+      //    minFrameRate: 30.0
+      //  }
+      //},
+      video: true,
       audio: false
     };
 
